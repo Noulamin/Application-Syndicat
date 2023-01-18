cc24a2022c7b4378793f1b50825423dc35b3e1c2
Nouaman <89440867+Noulamin@users.noreply.github.com>
2023-01-17 16:17:45 -0800

    .

--- backend/routes/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const { signin, signout } = require("../controllers/authController.js")
-
-router.post('/login', signin)
-router.get('/logout', signout)
-
-module.exports = router
